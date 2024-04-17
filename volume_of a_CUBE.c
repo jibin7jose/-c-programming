eb54ed4a22d508922021b4eca28440fb2b79cca4
jibin jose <jibinjose884@gmail.com>
2024-04-17 06:55:51 +0530

    Add files via upload

---
+++ volume_of a_CUBE.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+//start the program
+int h,w,d,vol; //variables declaration
+h=10;w=12;d=8; //assign value to variables
+vol=h*w*d; //calculation using mathematical formula
+printf("The Volume of the cube is: %d",vol); //display the
+volume
+getch();
+//e
