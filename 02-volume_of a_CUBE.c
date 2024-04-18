8e55c7c23cb4abb6d2e401b68b4de172e27f38d6
jibin jose <jibinjose884@gmail.com>
2024-04-18 08:09:35 +0530

    Update and rename volume_of a_CUBE.c to 02-volume_of a_CUBE.c

--- 02-volume_of a_CUBE.c
+++ 02-volume_of a_CUBE.c
@@ -5,5 +5,6 @@ void main()
 int h,w,d,vol; //variables declaration
 h=10;w=12;d=8; //assign value to variables
 vol=h*w*d; //calculation using mathematical formula
-printf("The Volume of the cube is: %d",vol); //display thevolume
+printf("The Volume of the cube is: %d",vol); //display the
+volume
 }
